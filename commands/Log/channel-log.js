848605a7e06bb2a4f14d1dca2956b377733b6374
İsmail Ayaz <44402226+MrRubby@users.noreply.github.com>
2023-11-14 04:53:12 +0300

    MUpdate
    Minor Update

--- commands/Log/channel-log.js
+++ commands/Log/channel-log.js
@@ -3,59 +3,57 @@ import { t } from "i18next"
 import database from "../../utils/database/guilds_Schema.js"
 
 export const data = {
-    name: "channel-log",
+    name: "channellog",
     description: "Sets the channel log channel",
 
     async execute(interaction) {
 
-        const menuName = t("channellog.menuName", { lng: interaction.locale})
+        let channel = interaction.options.getChannel("channel")
+        if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator))
+        return interaction.reply({ content: t("missing_permissions", {ns: "common", lng: interaction.locale}) })
 
-        const channelMenu = new ChannelSelectMenuBuilder()
-        .setCustomId(interaction.id)
-        .setPlaceholder(menuName)
-        .setMaxValues(1)
-        .setChannelTypes(ChannelType.GuildText)
+        let SubCmd = interaction.options.getSubcommand()
 
-        const Embed = new EmbedBuilder()
-        .setDescription(t("channellog.embed", {lng: interaction.locale}))
-        .setColor("Purple")
-
-        const SelectActionRow = new ActionRowBuilder().setComponents(channelMenu)
-
-        const reply = await interaction.reply({ embeds: [Embed], components: [SelectActionRow] })
-
-        const collector = reply.createMessageComponentCollector({
-            componentType: ComponentType.ChannelSelect,
-            filter: (i) => i.user.id === interaction.user.id && i.customId === interaction.id,
-            time: 60_000,
-        })
-
-        collector.on("collect",async (interaction) => {
-
-            if(interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-
-                await database.updateOne({ guild_id: interaction.guild.id }, { channellog_id: interaction.values.join() }, { upsert: true })
-                interaction.reply({ content: t("channellog.set_succes", {lng: interaction.locale}) })
-
+        switch(SubCmd) {
+            case "set" : {
+                await database.updateOne({ guild_id: interaction.guild.id}, {channellog_id: channel.id}, {upsert: true})
+                interaction.reply(t("channellog.set_succes", {lng: interaction.locale}))
                 const setEmbed = new EmbedBuilder()
-                .setDescription(t("channellog.set_embed", {lng: interaction.locale}))
-                .setColor("Blue")
-
-                const { channellog_id } = await database.findOne({ guild_id: interaction.guild.id })
-                const channelLog = interaction.guild.channels.cache.get(channellog_id)
-
-                channelLog.send({ embeds: [setEmbed] })
-
-            } else {
-
-                interaction.reply({ content: t("missing_permissions", {ns: "common", lng: interaction.locale}) })
-    
+                .setDescription(t("channellog.set_embed", {lng: interaction}))
+                channel.send({ embeds: setEmbed })
+                break
             }
-        })
+            case "reset" : {
+                await database.updateOne({ guild_id: interaction.guild.id}, {channellog_id: null}, {upsert: true})
+                interaction.reply({ content: t("channellog.res_succes", {lng: interaction.locale})})
+                break
+            }
+        }
     }
 }
 
 export const slash_data = {
     name: data.name,
     description: data.description,
+    options: [
+        {
+            name: "set",
+            description: "make a setup",
+            type: 1,
+            options: [
+                {
+                    name: "channel",
+                    description: "identify the channel",
+                    type: 7,
+                    required: true
+
+                }
+            ]
+        },
+        {
+            name: "reset",
+            description: "Resets the log system",
+            type: 1
+        }
+    ]
 }
