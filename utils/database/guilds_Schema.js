7d17ae9a58ef167cc2d88a0fa69ca6e701efd563
İsmail Ayaz <44402226+MrRubby@users.noreply.github.com>
2023-11-25 02:33:50 +0300

    Update guilds_Schema.js

--- utils/database/guilds_Schema.js
+++ utils/database/guilds_Schema.js
@@ -3,39 +3,39 @@ import mongoose from "mongoose"
 export default mongoose.model("guilds", new mongoose.Schema({
 
     // Initialize General
-    guild_id: { type: String, unique: false, required: true },
+    guild_id: { type: String, unique: true, required: true },
     // Initialize Log
-    modlog_id: { type: String, unique: false, required: true },
-    welcomelog_id: { type: String, unique: false, required: true },
-    rolelog_id: { type: String, unique: false, required: true },
-    messagelog_id: { type: String, unique: false, required: true },
-    channellog_id: { type: String, unique: false, required: true },
+    modlog_id: { type: String, unique: false, required: true, default: "" },
+    welcomelog_id: { type: String, unique: false, required: true, default: "" },
+    rolelog_id: { type: String, unique: false, required: true, default: "" },
+    messagelog_id: { type: String, unique: false, required: true, default: "" },
+    channellog_id: { type: String, unique: false, required: true, default: "" },
     // Initialize Ban
-    banrole_id: { type: String, unique: false, required: true },
-    bansystem: { type: Boolean, unique: false, required: true },
+    banrole_id: { type: String, unique: false, required: true, default: "" },
+    bansystem: { type: Boolean, unique: false, required: true, default: false },
     // Initialize Kick
-    kickrole_id: { type: String, unique: false, required: true },
-    kicksystem: { type: Boolean, unique: false, required: true },
+    kickrole_id: { type: String, unique: false, required: true, default: "" },
+    kicksystem: { type: Boolean, unique: false, required: true, default: false },
     //Initialize Register
-    adminrole_id: { type: String, unique: false, required: true},
-    userrole_id: { type: String, unique: false, required: true},
-    registerlog_id: { type: String, unique: false, required: true},
-    registersystem: { type: Boolean, unique: false, required: true},
+    adminrole_id: { type: String, unique: false, required: true, default: "" },
+    userrole_id: { type: String, unique: false, required: true, default: "" },
+    registerlog_id: { type: String, unique: false, required: true, default: "" },
+    registersystem: { type: Boolean, unique: false, required: true, default: false },
     // Initialize Autorole
-    autorole_id: { type: String, unique: false, required: true},
-    autorolesystem: { type: Boolean, unique: false, required: true},
-    autorolelog_id: { type: String, unique: false, required: true},
+    autorole_id: { type: String, unique: false, required: true, default: "" },
+    autorolesystem: { type: Boolean, unique: false, required: true, default: false},
+    autorolelog_id: { type: String, unique: false, required: true, default: "" },
     // Initialize Filter
-    kfrEngel: { type: Boolean, unique: false, required: true},
-    lnkEngl: { type: Boolean, unique: false, required: true},
-    kelimeEngl: { type: Boolean, unique: false, required: true},
-    bKlm: { type: String, unique: false, required: true},
+    kfrEngel: { type: Boolean, unique: false, required: true, default: false},
+    lnkEngl: { type: Boolean, unique: false, required: true, default: false},
+    kelimeEngl: { type: Boolean, unique: false, required: true, default: false},
+    bKlm: { type: String, unique: false, required: true, default: "" },
     // Initialize Reaction Roles
-    role: { type: String, unique: false, required: true},
-    roleDesc: { type: String, unique: false, required: true},
-    roleEmo: { type: String, unique: false, required: true},
+    role: { type: String, unique: false, required: true, default: "" },
+    roleDesc: { type: String, unique: false, required: true, default: "" },
+    roleEmo: { type: String, unique: false, required: true, default: "" },
     // Initialize Security
-    channelProtection: { type: Boolean, unique: false, required: true},
-    channelProtectionLog: { type: String, unique: false, required: true},
-    channelProtectionUserPoint: { type: String, unique: false, require: true},
+    channelProtection: { type: Boolean, unique: false, required: true, default: false},
+    channelProtectionLog: { type: String, unique: false, required: true, default: "" },
+    channelProtectionUserPoint: { type: String, unique: false, require: true, default: "" },
 }))
