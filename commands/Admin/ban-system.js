090734513ec6d72827ad0a9321257bb2b236e2b6
İsmail Ayaz <44402226+MrRubby@users.noreply.github.com>
2023-11-24 19:41:28 +0300

    minor update

--- commands/Admin/ban-system.js
+++ commands/Admin/ban-system.js
@@ -16,6 +16,8 @@ export const data = {
         switch(SubCmd){
             case "set": {
                 let rol = interaction.options.getRole("role");
+                if (!interaction.guild.members.me.roles.highest.position < rol.position)
+                return interaction.reply({ content: t("botrole_position_error", { ns: "error", lng: interaction.locale}), ephemeral: true })
                 await database.findOneAndUpdate({ guild_id: interaction.guild.id }, { $set: { banrole_id: rol.id, bansystem: true } }, { upsert: true })
                 .then(() => {
                     interaction.reply({ content: t("bansystem.set_succes", {lng: interaction.locale}) })
